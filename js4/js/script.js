21e680ad058a342adf2cb31338fa648877c09539
Katerina  Chebakova <katerina-2310@ya.ru>
2016-03-18 16:07:14 +0200

    add js4

---
+++ js4/js/script.js
@@ -0,0 +1,9 @@
+var wrapper = document.createElement('div');
+wrapper.classList.add('wrapper');
+
+var titleTest = document.createElement('div');
+titleTest.classList.add('title1');
+titleTest.innerHTML='Тест по программированию';
+var wrapper = document.querySelector('.wrapper');
+wrapper.app
+
