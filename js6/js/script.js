cde0579b04b2c87375967608b83baa682ab9ac92
Katerina  Chebakova <katerina-2310@ya.ru>
2016-03-22 14:32:25 +0200

    add js6

--- js6/js/script.js
+++ js6/js/script.js
@@ -1,6 +1,6 @@
 function handlerStart(){
   var buttonName = this.innerHTML;
-  if (buttonName ==='PAUSE') {
+  if (buttonName === 'PAUSE') {
     this.innerHTML = 'CONTINUE';
   } else {
     this.innerHTML = 'PAUSE';
@@ -19,3 +19,36 @@ var buttonClear = document.getElementsByClassName('btn-danger');
 buttonClear[0].addEventListener("click", handlerClear);
 
 var timer = document.getElementsByClassName('timer');
+console.log (timer);
+
+var time = {
+	hour: 0,
+	min: 0,
+	sec: 0,
+	
+	clear : function() {
+		this.hour = 00;
+		this.min = 00;
+		this.sec = 00;
+	},
+	incSec : function() {
+		this.sec++;
+		if (this.sec == 60){
+			this.sec = 00;
+			this.min++;
+			if (this.min==60){
+				this.min = 00;
+				this.hour++;
+	}}
+	},
+	toString: function() {
+		return this.hour+':'+this.min+':'+this.sec;
+	}
+}
+
+function startTimer(){
+	time.incSec();
+	timer[0].innerHTML = time.toString();
+}
+
+setInterval(startTimer,1000);
