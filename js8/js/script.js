82ec6d6797d4f8a814bd416bae57b3b39a5f95d4
Katerina  Chebakova <katerina-2310@ya.ru>
2016-04-01 15:43:55 +0300

    js8

--- js8/js/script.js
+++ js8/js/script.js
@@ -1,4 +1,11 @@
 $(function () {
 	var tabsText = $('.text div');
 	tabsText.hide().filter(':first').show();
+	
+	$('.menu li').on('click', function () {
+		var $tab = $(this).siblings('tabsText');
+		event.preventDefault()
+		$tab.toggle();
+	});
+	
 });
