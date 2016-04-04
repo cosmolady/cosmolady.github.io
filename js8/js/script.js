75a6ff9bdf3f5ff1d5e7482271787202224a6be2
Cosmolady <chebakovvl@gmail.com>
2016-04-04 13:55:50 +0300

    add js8

--- js8/js/script.js
+++ js8/js/script.js
@@ -1,11 +1,20 @@
 $(function () {
 	var tabsText = $('.text div');
+    var tabsMenu = $('.menu li');
+
 	tabsText.hide().filter(':first').show();
-	
+    
 	$('.menu li').on('click', function () {
-		var $tab = $(this).siblings('tabsText');
-		event.preventDefault()
-		$tab.toggle();
-	});
-	
+        var i = findPosition(this);
+        tabsText.hide().filter(tabsText[i]).fadeIn();
+        $(this).css('color','darkorchid');
+    });
+    
+	function findPosition(curLi) {
+        for (var i = 0; i < tabsMenu.length; i++) {
+            if(tabsMenu[i] === curLi){
+                return i;
+            }
+        }
+    }
 });
