50bdaf881bcf19e765b36d5cf3852f19a6ddb328
Katerina  Chebakova <katerina-2310@ya.ru>
2016-04-06 12:49:08 +0300

    js8

--- js8-2/js/script.js
+++ js8-2/js/script.js
@@ -1,11 +1,17 @@
 $(function () {
 	var $formField = $('input:text');
-	$formField.focus(function () {
+	$formField.mouseover(function () {
 		var name = $(this).attr('name');
 		$('label.hint[for="' + name + '"]').fadeIn();
 	});
-	$formField.blur(function () {
+	$formField.mouseout(function () {
 		var name = $(this).attr('name');
 		$('label.hint[for="' + name + '"]').hide();
+	});
+	var $buttonHelp = $('button:button');
+	var $textHelp = $('label .hint');
+	console.log ($textHelp);
+	$buttonHelp[0].click(function () {
+		$textHelp.fadeIn();
 	});
 });
