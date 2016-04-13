5868de1caeeb184ab208134e92bf8ac25026c9c9
Katerina  Chebakova <katerina-2310@ya.ru>
2016-04-13 11:44:35 +0300

    fix js10-2

--- js10-2/js/script.js
+++ js10-2/js/script.js
@@ -1,9 +1,19 @@
 $(function(){
 
-	$('.dropdown').on('hover',function() {
-	   $('.submenu').toggle();
+	var $dropdownMenu = $('.dropdown');
+	console.log ($dropdownMenu);
 	
+	var $submenuList =  $('.submenu');
+		console.log ($submenuList);
+
+	$dropdownMenu.on('mouseover',function() {
+	   $(this).children($dropdownMenu).slideDown(1000, stop()); 
 	});
 	
+	$dropdownMenu.on('mouseout',function() {
+	   $(this).children($dropdownMenu).slideUp(1000, stop()); 
+	});
 	
 })
+	
+	
