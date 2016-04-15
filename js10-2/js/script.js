5a13bea1540745e3f217afdf2d9cfecd90062659
Katerina  Chebakova <katerina-2310@ya.ru>
2016-04-15 11:18:05 +0300

    fix js10-2

--- js10-2/js/script.js
+++ js10-2/js/script.js
@@ -1,28 +1,28 @@
-$(function(){
+$(function () {
 	$('.dropdown').hover(
-		function(){
-			$(this).children('.submenu').slideDown(400,stop()); 
+		function () {
+			$(this).children('.submenu').slideDown(400, stop());
 		},
-		function(){
-			$(this).children('.submenu').slideUp(400,stop()); 
+		function () {
+			$(this).children('.submenu').slideUp(400, stop());
 		}
 	);
-	function stop(){
+
+	function stop() {
 		$('.submenu').stop(true, true);
 	};
-	
 
-	$('li').mouseover(
-		function (){
-    		$(this).animate({
-				backgroundColor:'#4B0082'
+
+	$('.submenu').hover(
+		function () {
+			$('.submenu').animate({
+				backgroundColor: '#F0E68C'
 			}, 500);
 		},
-		function(){
-			$(this).animate({
-				backgroundColor:'#0CF'
-			}, 500 );
+		function () {
+			$('.submenu').animate({
+				backgroundColor: '#FF7F50'
+			}, 500);
 		});
-
 
 });
