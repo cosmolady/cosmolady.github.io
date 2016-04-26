de056e0e5372e9d3d516824d3501693afffbb0ba
Katerina  Chebakova <katerina-2310@ya.ru>
2016-04-26 15:52:56 +0300

    fix js14

--- js14/js/script.js
+++ js14/js/script.js
@@ -1,40 +1,44 @@
 'use strict';
-$(function (){
+$(function () {
 
 
-var dataTest = [
-	{
-		title: 'Вопрос №1',
-		answers: ['Ответ 1', 'Ответ 2', 'Ответ 3']
+	var dataTest = [
+		{
+			title: 'Вопрос №1',
+			answers: ['Ответ 1', 'Ответ 2', 'Ответ 3'],
+			check: [false, true, false]
 	},
-	{
-		title: 'Вопрос №2',
-		answers: ['Ответ 1', 'Ответ 2', 'Ответ 3']
+		{
+			title: 'Вопрос №2',
+			answers: ['Ответ 1', 'Ответ 2', 'Ответ 3'],
+			check: [true, true, false]
 	},
-	{
-		title: 'Вопрос №3',
-		answers: ['Ответ 1', 'Ответ 2', 'Ответ 3']
+		{
+			title: 'Вопрос №3',
+			answers: ['Ответ 1', 'Ответ 2', 'Ответ 3'],
+			check: [false, false, true]
 	},
 ];
 
-localStorage.setItem('testQuestions',JSON.stringify(dataTest));
-var test = localStorage.getItem('testQuestions');
-var testObj = JSON.parse (test);
+	localStorage.setItem('testQuestions', JSON.stringify(dataTest));
+	var test = localStorage.getItem('testQuestions');
+	var testObj = JSON.parse(test);
 
-var page = $('#list-template').html();
-var testList = tmpl(page, {data: testObj});
-$('body').append(testList);
-	
+	var page = $('#list-template').html();
+	var testList = tmpl(page, {
+		data: testObj
+	});
+	$('body').append(testList);
+
+	var buttonCheck = $(":submit");
+
+	var mark = 0;
+
+	buttonCheck.click(function () {
+		if ($(this).prop('checked') === ($(this).attr('value') === true)) {
+			alert('hi');
+			mark++;
+		}
+	})
+	console.log(mark);
 })
-//
-//	
-//
-//
-//
-//var page = $('#list-template').html();
-//
-//var testList = tmpl(page, {data: testObj});
-//
-//$('body').append(testList);
-//	
-//})
