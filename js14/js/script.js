cc247297e7bdca359f0252e3466e42e094258524
Katerina  Chebakova <katerina-2310@ya.ru>
2016-04-27 16:02:43 +0300

    fix js14

--- js14/js/script.js
+++ js14/js/script.js
@@ -6,39 +6,56 @@ $(function () {
 		{
 			title: 'Вопрос №1',
 			answers: ['Ответ 1', 'Ответ 2', 'Ответ 3'],
-			check: [false, true, false]
+			rightAns: [false, true, false]
 	},
 		{
 			title: 'Вопрос №2',
 			answers: ['Ответ 1', 'Ответ 2', 'Ответ 3'],
-			check: [true, true, false]
+			rightAns: [true, true, false]
 	},
 		{
 			title: 'Вопрос №3',
 			answers: ['Ответ 1', 'Ответ 2', 'Ответ 3'],
-			check: [false, false, true]
+			rightAns: [false, false, true]
 	},
-];
+	];
+localStorage.setItem('testQuestions', JSON.stringify(dataTest));
+var test = localStorage.getItem('testQuestions');
+var testObj = JSON.parse(test);
 
-	localStorage.setItem('testQuestions', JSON.stringify(dataTest));
-	var test = localStorage.getItem('testQuestions');
-	var testObj = JSON.parse(test);
+var page = $('#list-template').html();
+var testList = tmpl(page, {
+	data: testObj
+});
+$('body').append(testList);
 
-	var page = $('#list-template').html();
-	var testList = tmpl(page, {
-		data: testObj
-	});
-	$('body').append(testList);
+var buttonCheck = $(":submit");
 
-	var buttonCheck = $(":submit");
+var mark = 0;
 
-	var mark = 0;
+buttonCheck.click(function () {
+	checkAnswer();
 
-	buttonCheck.click(function () {
-		if ($(this).prop('checked') === ($(this).attr('value') === true)) {
-			alert('hi');
-			mark++;
+})
+
+function checkAnswer() {
+	// считаем что ответы правильные
+	var error = false;
+	// чекбоксы
+	var inputs = $("input[value='0']");
+	
+	for (var i = 0; i < inputs.length; i++) {
+		// галка
+		var checked = $("input[value='0']:checked");
+			
+		// является ли вариант правильным
+		var right = testObj[i].rightAns[i] == true;
+		// если отметка не является правильной
+		if (checked !== right) {
+			error = true;
+			// дальше можно не проверять
+			break;
 		}
-	})
-	console.log(mark);
+	}
+console.log(error ? 'Ошибка!!!' : 'Зачот!!!')}
 })
