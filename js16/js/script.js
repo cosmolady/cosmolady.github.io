254ecf0bb4a4077ae8ff88f0abba0afd44a97dfa
Katerina  Chebakova <katerina-2310@ya.ru>
2016-05-06 13:08:39 +0300

    add js16

---
+++ js16/js/script.js
@@ -0,0 +1,19 @@
+$(function () {
+
+	// callback function
+	function GoogleCallback(func, data) {
+		window[func](data);
+	}
+	$.getJSON("http://ajax.googleapis.com/ajax/services/search/web?v=1.0?key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=PHP&callback=GoogleCallback&context=?",
+
+		function (data) {
+			var ul = document.createElement("ul");
+			$.each(data.results, function (i, val) {
+				var li = document.createElement("li");
+				li.innerHTML = '<a href="' + val.url + '" title="' + val.url + '" target="_blank">' + val.title + "</a> - " + val.content;
+				ul.appendChild(li);
+			});
+			$('.gs-result').html(ul);
+		});
+
+})
