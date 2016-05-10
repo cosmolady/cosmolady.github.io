cb2a36ff800d783a551e7b4c2ad0bccd361f3f10
Katerina  Chebakova <katerina-2310@ya.ru>
2016-05-10 14:01:16 +0300

    add js16

--- js16/js/script.js
+++ js16/js/script.js
@@ -1,11 +1,53 @@
 /*Поиск Google*/
-function hndlr(response) {
-      for (var i = 0; i < response.items.length; i++) {
-        var item = response.items[i];
-        // in production code, item.htmlTitle should have the HTML entities escaped.
-        document.getElementById("content").innerHTML += "<br>" + item.htmlTitle;
-      }
-    }
-
+$.getJSON("https://www.googleapis.com/customsearch/v1?key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&callback=?",
+function(data){
+    var ul = document.createElement("ul");
+    $.each(data.results, function(i, val){
+            var li = document.createElement("li");
+            li.innerHTML = '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+"</a> - "+val.content;                            
+            ul.appendChild(li);
+    });
+    $('.gs-result').html(ul);
+});
 /*Prototypes*/
+
+function Human() {
+	this.name =  'Ivan';
+	this.age = 30;
+	this.sex = 'male';
+	this.height = 180;
+	this.weight = 70;
+	
+}
+
+Worker.prototype = new Human();
+Student.prototype = new Human();
+
+function Worker () {
+	this.job = 'manager';
+	this.salary = '500$';
+	this.method = "Let's work!";
+}
+
+function Student (){
+	this.univercity = 'Oxford';
+	this.grants = '100$';
+	this.method = "Let's watch series";
+}
+
+worker1 = new Worker();
+worker2 = new Worker();
+
+stud1 = new Student();
+stud2 = new Student();
+
+console.log (worker1.job);
+console.log (worker2.name);
+console.log (worker1.height);
+console.log (worker2.method);
+
+console.log (stud1.grants);
+console.log (stud1.age);
+console.log (stud2.method);
+console.log (stud2.sex);
 
