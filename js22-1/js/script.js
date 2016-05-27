b497c3a28ac1a9e322cac2931a26e44061166910
Katerina  Chebakova <katerina-2310@ya.ru>
2016-05-27 11:46:59 +0300

    add js22-1

--- js22-1/js/script.js
+++ js22-1/js/script.js
@@ -1,5 +1,5 @@
 var app = {
-	function pow(x, n) {
+	pow : function(x, n) {
   var result = 1;
 
   if (n < 0) {
@@ -16,5 +16,5 @@ var app = {
 //  var x = prompt("Enter number", "");
 //  var n = prompt("Enter power", "");
 
-  console.log ('result =', pow(x,n));
- module.exports = pow;
+//  console.log ('result =', pow(x,n));
+ module.exports = app;
