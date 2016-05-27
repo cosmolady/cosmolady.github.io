99d504d9e61dc06f7d8a45601b04c5ac90d649e6
Katerina  Chebakova <katerina-2310@ya.ru>
2016-05-27 12:04:22 +0300

    add js22-1

--- js22-1/spec.js
+++ js22-1/spec.js
@@ -1,6 +1,6 @@
 var app = require('../js/script.js');
 describe ("app",function(){
-	it("should call  pow method", function(){
+	it("should raise to the power", function(){
 		var result;
 		result=app.pow(5,2);
 	expect(result).toBe(25);
