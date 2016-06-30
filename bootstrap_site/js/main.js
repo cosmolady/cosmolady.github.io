c0593a1740b3b254cc848524faf86742d5e17d3a
Katerina  Chebakova <katerina-2310@ya.ru>
2016-06-30 16:08:09 +0300

    fix site

--- bootstrap_site/js/main.js
+++ bootstrap_site/js/main.js
@@ -7,7 +7,26 @@ jQuery(function($) {'use strict',
 		});
 	});
 
-
+//$("form").submit(function () {
+// // Получение ID формы
+// var formID = $(this).attr('id');
+// // Добавление решётки к имени ID
+// var formNm = $('#' + formID);
+// $.ajax({
+// type: "POST",
+// url: 'mail.php',
+// data: formNm.serialize(),
+// success: function (data) {
+// // Вывод текста результата отправки
+// $(formNm).html(data);
+// },
+// error: function (jqXHR, text, error) {
+// // Вывод текста ошибки отправки
+// $(formNm).html(error);
+// }
+// });
+// return false;
+// });
 	// accordian
 	$('.accordion-toggle').on('click', function(){
 		$(this).closest('.panel-group').children().each(function(){
--- bootstrap_site/sendemail.php
+++ bootstrap_site/sendemail.php
@@ -9,11 +9,12 @@
     $email = @trim(stripslashes($_POST['email'])); 
     $subject = @trim(stripslashes($_POST['subject'])); 
     $message = @trim(stripslashes($_POST['message'])); 
+	$phone  = @trim(stripslashes($_POST['phone']));
 
     $email_from = $email;
     $email_to = 'katerina-2310@ya.ru';//replace with your email
 
-    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
+    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message.  'Phone: ' . $phone;
 
     $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
 
