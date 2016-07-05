7f705327b65f3d108e0bb84a01b86f484e0b4027
Katerina  Chebakova <katerina-2310@ya.ru>
2016-07-05 16:36:50 +0300

    add wdj site

---
+++ wdj/wdj_files/main.js
@@ -0,0 +1,8 @@
+$(function(){
+	$(".button").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
