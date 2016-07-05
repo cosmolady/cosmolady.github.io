2541fd42777d3c22e2d8ba5e7f74dac9a21c9ec4
server <katerina-2310@ya.ru>
2016-07-05 22:57:23 +0300

    fix site

--- wdj/wdj_files/main.js
+++ wdj/wdj_files/main.js
@@ -1,8 +1,30 @@
-$(function(){
-	$(".button").on("click","a", function (event) {
-		event.preventDefault();
-		var id  = $(this).attr('href'),
-		top = $(id).offset().top;
-		$('body,html').animate({scrollTop: top}, 1500);
-	});
+$(function () {
+    $(".button").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 1500);
+    });
+
+    $('.login-email').on("click", function (e) {
+        e.preventDefault();
+        var elemHr = $(this).attr("href");
+        var top = $(elemHr).offset().top;
+        
+        $('body,html').animate({
+            scrollTop: top
+        }, 1500);
+
+        $('#remind-form').slideUp(600, 'easeInSine');
+
+        $('#login-form').slideToggle(600, 'easeInSine');
+    })
+    $('.forgot').click(function (e) {
+        event.preventDefault();
+
+        $('#login-form').slideUp(600, 'easeInSine');
+        $('#remind-form').slideToggle(600, 'easeInSine');
+    })
 });
