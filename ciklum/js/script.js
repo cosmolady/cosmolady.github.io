1c1ebff9715101a97d00402170bf44cf046cec7a
Katerina  Chebakova <katerina-2310@ya.ru>
2016-07-13 16:12:04 +0300

    add all

--- ciklum/js/script.js
+++ ciklum/js/script.js
@@ -1,4 +1,21 @@
 $(function () {
+	$(document).ready(function () {
+		$('a').on('click', function (e) {
+			e.preventDefault();
+		});
+
+		$('#ddmenu li').hover(function () {
+			clearTimeout($.data(this, 'timer'));
+			$('ul', this).stop(true, true).slideDown(200);
+		}, function () {
+			$.data(this, 'timer', setTimeout($.proxy(function () {
+				$('ul', this).stop(true, true).slideUp(200);
+			}, this), 100));
+		});
+
+	});
+
+
 	$dropdown = $('.shell-header-dropdown');
 	$itemSubmenu = $('.shell-header-dropdown-tab');
 
