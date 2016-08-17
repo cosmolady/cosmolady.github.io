531cc489530998da37722c460b68068f09e4e3db
chebakovvladimir <chebakovvl@gmail.com>
2016-08-17 23:20:44 +0300

    fix bug

--- book1bonus-changed/js/main.js
+++ book1bonus-changed/js/main.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
     $('.firstp h1').gradientText({
         colors: ['#fc7264', '#fe9952', '#fec64f']
     });
-    var arrow = $(".arrow");
-    var intervalId = startArrowJump(arrow);
+    var arrowBounce = $(".arrow_bounce");
+    var normalP = $('.normalp');
+    arrowBounce.effect("bounce", 4000);
+    setInterval(function () {
+        arrowBounce.effect("bounce", 4000);
+    }, 800);
     // Add smooth scrolling to all links
     $("a").on('click', function (event) {
         // Make sure this.hash has a value before overriding default behavior
@@ -16,29 +20,17 @@ $(document).ready(function () {
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 800, function () {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
+            }, 800, function () {});
         } // End if
         if ($(this).attr('href') == "#subscribe") {
             $(this).attr('href', '#up');
-            arrow.stop(true, true);
-            arrow.addClass('arrowup');
-            clearInterval(intervalId);
+            arrowBounce.addClass('arrowup');
+            normalP.css({'visibility':'hidden'});
         } else if ($(this).attr('href') == "#up") {
             $(this).attr('href', '#subscribe');
-            arrow.removeClass('arrowup');
-            intervalId = startArrowJump(arrow);
+            arrowBounce.removeClass('arrowup');
+            normalP.css({'visibility':'visible'});
         }
     });
-
-    function startArrowJump(arrow) {
-        //arrow.effect("bounce", 4000);
-        return setInterval(function () {
-            arrow.effect("bounce", 4000);
-        }, 4000);
-    }
 });
 //document.body.addEventListener('touchmove', function(e){ e.preventDefault(); });
