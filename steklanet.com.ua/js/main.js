0894d1eadccfd7b421190a65bd58120be62b736d
chebakovvladimir <chebakovvl@gmail.com>
2016-08-17 09:17:29 +0300

    add message

--- steklanet.com.ua/js/main.js
+++ steklanet.com.ua/js/main.js
@@ -1,110 +1,113 @@
-jQuery(function($) {'use strict',
+jQuery(function ($) {
+    'use strict',
 
-	
-	// all Parallax Section
-	$(window).load(function(){'use strict',
-		$("#services").parallax("50%", 0.3);
-		$("#clients").parallax("50%", 0.3);
-	});
-	
-	// portfolio filter
-	$(window).load(function(){'use strict',
-		$portfolio_selectors = $('.portfolio-filter >li>a');
-		if($portfolio_selectors!='undefined'){
-			$portfolio = $('.portfolio-items');
-			$portfolio.isotope({
-				itemSelector : '.col-sm-3',
-				layoutMode : 'fitRows'
-			});
-			
-			$portfolio_selectors.on('click', function(){
-				$portfolio_selectors.removeClass('active');
-				$(this).addClass('active');
-				var selector = $(this).attr('data-filter');
-				$portfolio.isotope({ filter: selector });
-				return false;
-			});
-		}
-	});
-	
-	//Pretty Photo
-	 $("a[data-gallery^='prettyPhoto']").prettyPhoto({
-	  social_tools: false
-	 });
+    // all Parallax Section
+    $(window).load(function () {
+        'use strict',
+        $("#services").parallax("50%", 0.3);
+        $("#clients").parallax("50%", 0.3);
+    });
 
+    // Contact form validation
+    var contactForm = $('#contact-form-section');
+    var lastMessage = $('.last_message');
+    var errorMessage = $('.error_message');
+    $('.btn-send').on('click', function (e) {
+        'use strict';
+        var spinner = new Spinner().spin();
+        $.ajax({
+            url: '/steklanet.com.ua/send-mail.php',
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                name: $('.name-field').val(),
+                phone: $('.mail-field').val(),
+                message: $('.message-field').val()
+            },
+            success: function (data) {
+                contactForm.addClass('none');
+                lastMessage.removeClass('none');
+                spinner.stop();
+            },
+            error: function (data) {
+                errorMessage.removeClass('none');
+                console.log(data);
+                spinner.stop();
+            }
+        });
+        return false;
+    });
 
-	// Contact form validation
-	var form = $('.contact-form');
-	form.submit(function () {'use strict',
-		$this = $(this);
-		$.post($(this).attr('action'), function(data) {
-			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
-		},'json');
-		return false;
-	});
+    $('.btn-default').on('click', function (e) {
+        'use strict';
+        lastMessage.addClass('none');
+        errorMessage.addClass('none');
+        contactForm.removeClass('none');
+        $('#contact-form').trigger('reset');
+    })
 
+    // Navigation Scroll
+    $(window).scroll(function (event) {
+        Scroll();
+    });
 
-	// Navigation Scroll
-	$(window).scroll(function(event) {
-		Scroll();
-	});
-
-	$('.navbar-collapse ul li a').click(function() {  
-		$('html, body').animate({scrollTop: $(this.hash).offset().top - 79}, 1000);
-		return false;
-	});
-
+    $('.navbar-collapse ul li a').click(function () {
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - 79
+        }, 1000);
+        return false;
+    });
 });
 
 // Preloder script
-jQuery(window).load(function(){'use strict';
-	$(".preloader").delay(1600).fadeOut("slow").remove();
+jQuery(window).load(function () {
+    'use strict';
+    $(".preloader").delay(1600).fadeOut("slow").remove();
 });
 
 //Preloder script
-jQuery(window).load(function(){'use strict';
+jQuery(window).load(function () {
+    'use strict';
+    // Slider Height
+    var slideHeight = $(window).height();
+    $('#home .carousel-inner .item, #home .video-container').css('height', slideHeight);
 
-	// Slider Height
-	var slideHeight = $(window).height();
-	$('#home .carousel-inner .item, #home .video-container').css('height',slideHeight);
-
-	$(window).resize(function(){'use strict',
-		$('#home .carousel-inner .item, #home .video-container').css('height',slideHeight);
-	});
+    $(window).resize(function () {
+        'use strict',
+        $('#home .carousel-inner .item, #home .video-container').css('height', slideHeight);
+    });
 
 });
 
 
 // User define function
 function Scroll() {
-	var contentTop      =   [];
-	var contentBottom   =   [];
-	var winTop      =   $(window).scrollTop();
-	var rangeTop    =   200;
-	var rangeBottom =   500;
-	$('.navbar-collapse').find('.scroll a').each(function(){
-		contentTop.push( $( $(this).attr('href') ).offset().top);
-		contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
-	})
-	$.each( contentTop, function(i){
-		if ( winTop > contentTop[i] - rangeTop ){
-			$('.navbar-collapse li.scroll')
-			.removeClass('active')
-			.eq(i).addClass('active');			
-		}
-	})
+    var contentTop = [];
+    var contentBottom = [];
+    var winTop = $(window).scrollTop();
+    var rangeTop = 200;
+    var rangeBottom = 500;
+    $('.navbar-collapse').find('.scroll a').each(function () {
+        contentTop.push($($(this).attr('href')).offset().top);
+        contentBottom.push($($(this).attr('href')).offset().top + $($(this).attr('href')).height());
+    })
+    $.each(contentTop, function (i) {
+        if (winTop > contentTop[i] - rangeTop) {
+            $('.navbar-collapse li.scroll')
+                .removeClass('active')
+                .eq(i).addClass('active');
+        }
+    })
 
 };
 
 
-	// Skill bar Function
+// Skill bar Function
 
-	jQuery(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},6000);
-	});
+jQuery(document).ready(function () {
+    jQuery('.skillbar').each(function () {
+        jQuery(this).find('.skillbar-bar').animate({
+            width: jQuery(this).attr('data-percent')
+        }, 6000);
+    });
 });
-
-
--- steklanet.com.ua/send-mail.php
+++ steklanet.com.ua/send-mail.php
@@ -10,9 +10,9 @@ $body = "Name: " . $name . "\n";
 $body .= "Phone: " . $phone . "\n"; 
 $body .= "Message: " . $message . "\n"; 
 
-$client = new TurboSMS();
+//$client = new TurboSMS();
 
-$client->send("380504701909",$body);
+//$client->send("380504701909",$body);
  
 $mailSMTP = new SendMailSmtpClass('skorik71@mail.ru', 'bretty', 'ssl://smtp.mail.ru', 'steklanet.com.ua', 465);
   
@@ -20,16 +20,6 @@ $mailSMTP = new SendMailSmtpClass('skorik71@mail.ru', 'bretty', 'ssl://smtp.mail
 $headers= "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=utf-8\r\n"; 	// кодировка письма
 $headers .= "From: steklanet.com.ua <skorik71@mail.ru>\r\n";   			// от кого письмо
-$result =  $mailSMTP->send('skorik71@mail.ru', 'Заказ с stekla.net.com', $body, $headers); // отправляем письмо
+//$result =  $mailSMTP->send('skorik71@mail.ru', 'Заказ с stekla.net.com', $body, $headers); // отправляем письмо
+echo json_encode(array('status'=>'ok'));
 ?>
-
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-
-    <head>
-        <script>
-            alert("Your message has been sent successfully. We will contact you shortly.");
-        </script>
-        <meta HTTP-EQUIV="REFRESH" content="0; url=index.html">
-
-    </head>
