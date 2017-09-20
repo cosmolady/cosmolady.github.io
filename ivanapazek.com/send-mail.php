a73de5b6cc187b0189f97609134697111db244e7
Katerina Chebakova <katerina-2310@ya.ru>
2017-09-20 23:21:26 +0300

    del pass

--- ivanapazek.com/send-mail.php
+++ ivanapazek.com/send-mail.php
@@ -9,12 +9,12 @@ $body = "Name: " . $name . "\n";
 $body .= "Email: " . $email . "\n"; 
 $body .= "Message: " . $message . "\n"; 
  
-$mailSMTP = new SendMailSmtpClass('pazekivana@gmail.com', 'pazekivana8824', 'ssl://smtp.gmail.com', 'ivanapazek.com', 465);
+$mailSMTP = new SendMailSmtpClass('', '', 'ssl://smtp.gmail.com', 'ivanapazek.com', 465);
   
 
 $headers= "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=utf-8\r\n"; 	// кодировка письма
-$headers .= "From: ivanapazek.com <pazekivana@gmail.com>\r\n";   			// от кого письмо
-$result =  $mailSMTP->send('ivana.pazek@gmail.com', 'letter from ivanapazek.com', $body, $headers); // отправляем письмо
+$headers .= "From: ivanapazek.com <>\r\n";   			// от кого письмо
+$result =  $mailSMTP->send('', 'letter from ivanapazek.com', $body, $headers); // отправляем письмо
 echo json_encode(array('status'=>$result));
 ?>
